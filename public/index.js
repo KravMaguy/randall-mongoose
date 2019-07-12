965411b543216f8e2b249d1dfcaaf0563b7e6ae5
abe <greykrav@gmail.com>
2019-07-12 16:07:17 -0500

    added instructions

--- public/index.js
+++ public/index.js
@@ -1,10 +1,22 @@
 const displayComments = ({ comments }) => {
   const commentsElem = document.getElementById("comments");
   commentsElem.innerHTML += comments.reduce((html, comment) => {
+    console.log(comment)
+    console.log('  ----------------------  ')
     const commentDiv = `
       <div>${comment.email}</div>
       <div>${comment.comment}</div>
     `;
+    //the ul element will now have an id equal to what I passed back as part of the response
+    //and have a delete button with a class of delete, attach a click event to the button, so that 
+    //when you click and the event.target has a class of delete  (this will all be on the server side)
+    //that is when you have to get the id of the ul element (you will have to go up the chain of ancestors to find look into findthenextparent() or something)
+    //get the id from the parent, do two things
+    //1. use fetch to send the id in a delete request to get rid of it in the database, 
+    //2.and send a success response that you did it,
+    //3.ajnd then when you response back in the callback as long as its a success than remove the element from the dom without refreshing the page.
+    //4.notify the user that he did or did not delete it..use a modal or something
+    
     return html += `<li class="list-group-item">${commentDiv}</li>`;
   }, '');
 };
